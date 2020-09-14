37aa86f717a84fd3e853de385e78af407af22a0e
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-09-14 08:52:35 +0100

    right sidebar and twwet panel ok

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,10 @@
+<h3 class="font-bold text-lg mb-4">Friends</h3>
+<ul>
+  @foreach(range(1,8) as $index)
+  <li class="mb-4">
+    <div class="flex items-center text-sm"><img src="https://picsum.photos/40" alt="" class="rounded-full mr-2">Oussema
+      Sfèri
+    </div>
+  </li>
+  @endforeach
+</ul>

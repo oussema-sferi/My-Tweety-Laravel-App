bda72298f7258d39d9549033a989a18980d668da
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-09-14 14:29:48 +0100

    timeline expanded, see all followed users posts

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
+    public function index()
+    {
+        return view('home', ['tweets' => auth()->user()->timeline()]);
+    }
+
     public function store()
     {
         $attributes = request()->validate(['body' => 'required|max:255']);
